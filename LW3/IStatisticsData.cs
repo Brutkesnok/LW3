f28b0cc9b36d7de045a09344bc766dbed7f76be0
Brutkesnok <maig15sk@gmail.com>
2023-05-15 12:23:30 +0300

    Created IStatisticsData interface and added functionality to MainForm methods

---
+++ LW3/IStatisticsData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW3
+{
+    public interface IStatisticsData
+    {
+        bool LoadData(string filePath);
+
+        string[] GetFields();
+
+        List<string[]> GetEntries();
+
+        string GetSummary();
+    }
+}
